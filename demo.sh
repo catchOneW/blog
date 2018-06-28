141307449b6aaf2478c56d386be8f3faaf51888b
catchOneW <kaiqiw666@gmail.com>
2018-06-28 23:18:10 +0800

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,8 @@
+mkdir $1
+cd $1
+mkdir css js
+touch index.html css/style.css js/main.js
+echo   '<!DOCTYPE><title>Hello</title><h1>Hi</h1>' > index.html
+echo   'h1{color: red;}' > css/style.css
+echo   ' var string = "Hello World"alert(string)' >  js/main.js
+exit
