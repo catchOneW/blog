2cc14a5ef7a99ef208e1a15a470de548118d8805
catchOneW <kaiqiw666@gmail.com>
2018-06-30 11:28:50 +0800

    Add files via upload

---
+++ git.sh
@@ -0,0 +1,5 @@
+git config --global user.name 'kaiqiw'
+git config --global user.email '1132620342@qq.com'
+git config --global push.default matching 
+git config --global core.quotepath false
+git config --global core.editor "vim"
