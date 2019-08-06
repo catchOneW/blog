22aa003cebe66c3f945b4ef9d08895b16a8d3054
unknown <1132620342@qq.com>
2019-08-06 20:56:06 +0800

    ok

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/blog",
+  pathPrefix: "/blog/public",
   siteMetadata: {
     title: `kaiqiw blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
